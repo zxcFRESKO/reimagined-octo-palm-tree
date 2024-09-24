b1b6a1c4fbf25edd2856f8d7a73acd0eefbd937f
zxcFRESKO <139549258+zxcFRESKO@users.noreply.github.com>
2024-09-24 15:21:56 +0300

    Add files via upload

---
+++ assets/JS/login.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('.form__button')
+const input = document.querySelectorAll('.input')
+btn.addEventListener('click', function clearing(){
+   for(let i in input){
+    input[i].value = ''
+     input[i].value = ''
+      input[i].value = ''
+         input[i].value = ''       
+   }
+   alert('Вы отправили заявку')
+})
+    
